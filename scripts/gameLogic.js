56579f7dfb95cd61992cbb58927c56494cccda52
Aaron <aarongillies88@gmail.com>
2020-07-27 18:31:35 -0400

    adding logic for player to hit taking another card

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -1,7 +1,7 @@
 "use strict";
 // Need to bring in Player and Dealer.
-const dealer = require("./dealer");
-const player = require("./player");
+const Dealer = require("./dealer");
+const Player = require("./player");
 // Should initialize the deck as an array and populate with cards. (Gonna roll with Object Oriented Paradigm for this)
 class Deck {
   constructor() {
@@ -29,7 +29,17 @@ class Deck {
     while (numDecks < 6) {
       suits.forEach((suit) => {
         for (let v = 0; v < values.length; v++) {
-          this.deck.push({ cardSuit: suit, cardValue: values[v] });
+          let cardNumValue = parseInt(values[v]);
+          if (values[v] === "J" || values[v] === "Q" || values[v] === "K") {
+            cardNumValue = 10;
+          } else if (values[v] === "A") {
+            cardNumValue = 11;
+          }
+          this.deck.push({
+            cardSuit: suit,
+            cardType: values[v],
+            cardValue: cardNumValue,
+          });
         }
       });
       numDecks += 1;
@@ -58,7 +68,46 @@ class Deck {
   }
 }
 
-console.log(dealer);
-console.log(player);
+// Function to allow user to hit, taking another card.
+function hit(card, player) {
+  let nextCard = card;
+  let currentPlayer = player;
+  let handTotal = currentPlayer.hand.reduce((a, b) => a + b);
+
+  currentPlayer.hand.push(nextCard);
+  handTotal += nextCard.cardValue;
+
+  // Conditional logic to take a card (this function will be called if user presses the 'hit' button on the UI)
+  if (handTotal > 21) {
+    console.log("Bust!");
+    // Some kind of reset function to get ready for next hand
+  } else {
+    console.log("What is your next move?");
+  }
+}
+
+// Function to initialize game
+function gameStart() {
+  // Create Deck
+  const newDeck = new Deck();
+  // Shuffle Deck
+  newDeck.shuffle();
+  // Initialize Player and Dealer
+  const dealer = new Dealer();
+  const player = new Player("Aaron");
+
+  // Set current player to a variable
+  let currentPlayer = player;
+
+  // Deal cards to dealer and player (Player gets 1, then dealer, then player until both have 2 cards)
+  while (player.hand.length < 2 && dealer.hand.length < 2) {
+    if (player.hand.length < 2) {
+      player.hand.push(newDeck.deal());
+    }
+    if (dealer.hand.length < 2) {
+      dealer.hand.push(newDeck.deal());
+    }
+  }
+}
 
 module.exports = Deck;
